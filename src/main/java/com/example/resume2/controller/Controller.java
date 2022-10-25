55687eab0d8f3e1973f89e5a364824817b3e0595
ahmadali <ahmadaligulomov777@gmail.com>
2022-10-25 23:16:04 +0500

    Second Commit

--- src/main/java/com/example/resume2/controller/Controller.java
+++ src/main/java/com/example/resume2/controller/Controller.java
@@ -1,16 +1,25 @@
 package com.example.resume2.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.stereotype.Controller
 public class Controller {
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "index";
     }
 
     @GetMapping("/message")
-    public String message(){
+    public String message() {
         return "message";
+    }
+
+    @PostMapping("/send")
+    public String send(@RequestParam("firstName") String firstName, @RequestParam("message") String message) {
+        System.out.println(firstName);
+        System.out.println(message);
+        return "success";
     }
 }
